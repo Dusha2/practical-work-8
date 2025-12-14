8227cbcf96e3285cff6f41d8a866415c7aa3141a
RD_727 <110250717+Dusha2@users.noreply.github.com>
2025-12-14 12:12:59 +0200

    Remove translation functionality from Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,9 @@
-import { useTranslation } from "react-i18next";
 import type { FunctionComponent } from "../common/types";
 
 export const Home = (): FunctionComponent => {
-	const { t, i18n } = useTranslation();
-
-	const onTranslateButtonClick = async (): Promise<void> => {
-		if (i18n.resolvedLanguage === "en") {
-			await i18n.changeLanguage("es");
-		} else {
-			await i18n.changeLanguage("en");
-		}
-	};
-
 	return (
 		<p className="text-red-500 font-bold">
-  			Привіт! Це моя Лабораторна №7. HMR працює!
+			Привіт! Це моя Лабораторна №7. HMR працює!
 		</p>
 	);
 };
